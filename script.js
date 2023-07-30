319fbf25d0f2c70b93c2daf28265e01871bc4159
shivam <kumarshivam5527@gmail.com>
2023-07-30 23:44:43 +0530

    site completed

--- script.js
+++ script.js
@@ -38,3 +38,34 @@ function circleMouseFollower(){
 }
 
 circleMouseFollower()
+
+document.querySelectorAll('.element').forEach( function(element){
+
+    var rotate = 0;
+    var diffrot = 0;
+  
+    element.addEventListener("mouseleave", function () {
+      gsap.to(element.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+    element.addEventListener("mousemove", function(dets){
+
+        let diff = dets.clientY - element.getBoundingClientRect().top ;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        gsap.to(element.querySelector('img') ,{
+            opacity : 1 , 
+            ease: Power3,
+            top : diff ,
+            left : dets.clientX ,
+      duration: 0.5,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+
+        });
+    });
+    
+});
+
