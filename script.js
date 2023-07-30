33c2960e545307821bfd84b05dd9b3ee949ce643
shivam <kumarshivam5527@gmail.com>
2023-07-30 22:02:57 +0530

    adding animation on hero section

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim() {
+    var tl = gsap.timeline();
+  
+    tl.from("#nav", {
+      y: "-10",
+      opacity: 0,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+    })
+      .to(".bounding-element", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: 0.2,
+      })
+      .from("#hero-footer", {
+        y: 0,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+      });
+  }
+
+
+  firstPageAnim()
 function circleMouseFollower(){
     window.addEventListener("mousemove", 
     function(dets){
